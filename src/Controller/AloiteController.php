41b3514f2d6b751789a689a918c07ef60a7212d1
juttahy <jutta.hyrskylahti@gmail.com>
2019-10-04 13:00:11 +0300

    added AloiteController.php to the project

---
+++ src/Controller/AloiteController.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Entity\Linkki;
+use App\Form\LinkkiFormType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/* 
+- Aloitteen tekeminen
+•Aloitteen tulostaminen (tulosteeseen vain aihe, kuvaus ja kirjauspäiväys)
+•Aloitteen poistaminen
+•Aloitteen muokkaaminen
+•Yksittäisen aloitteen kaikkien tietojen näyttäminen
+*/
+
+
+
+class AloiteController extends AbstractController {
+
+
+
+}
+
+
+?>
